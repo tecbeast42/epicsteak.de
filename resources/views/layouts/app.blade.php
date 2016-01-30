893d092f3a0748af21ef3671631b14269a7f2898
T. Kriebisch <t.kriebisch@tecbeast.de>
2016-01-30 18:26:02 +0100

    IWP

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,10 @@
 
     <title>Epic Steak</title>
 
-    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
 </head>
 <body id="app-layout">
     <h1 class="text-center">Welcome to the Steak</h1>
-    <script src="{{ elixir('js/app.js') }}"></script>
+    {{-- // <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
